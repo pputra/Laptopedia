318b3a218b931c5a88f79c45ec2d94c6fcba8a96
pputra <mynameisyoga@live.com>
2018-09-27 14:20:43 +0700

    add Users and Products Models

---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
